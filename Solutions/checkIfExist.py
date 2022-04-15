851b73143a190c331d7576025aa50b22b78190e4
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-04-15 18:04:00 +0100

    day 7 completed

--- Solutions/checkIfExist.py
+++ Solutions/checkIfExist.py
@@ -6,7 +6,7 @@ def checkIfExist(self, arr):
   """
   hashSet= set()
   for i in range(len(arr)):
-    if arr[i]*2 in hashSet or (arr[i]%2 and arr[i]//2 in hashSet):
+    if arr[i]*2 in hashSet or (arr[i]%2==0 and arr[i]//2 in hashSet):
       return True
     hashSet.add(arr[i])
     
