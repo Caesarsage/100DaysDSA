3d5f8da7422229d801739c0ce7c677e766307182
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2022-10-02 13:25:59 +0100

    Added Two Sum Problem

--- Solutions/javascript/1. Two_Sum.js
+++ Solutions/javascript/1. Two_Sum.js
@@ -2,7 +2,8 @@
 1. Two Sum - https://leetcode.com/problems/two-sum/
 */
 
-// Approach 1. O(n * n)
+// Approach 1.
+// Time Complexity - O(n * n), Space Complexity - O(1)
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++)
         for (let j = 0; j < nums.length; j++) 
@@ -10,10 +11,32 @@ var twoSum = function(nums, target) {
                 return [i, j];
 }; 
 
-// Approach 2. O(n)
+// Approach 2.
+// Time Complexity - O(n ), Space Complexity - O(1)
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) { 
          let indexOfDiff = nums.indexOf(target - nums[i]);
          if (indexOfDiff !== -1 && indexOfDiff !== i) return [i, indexOfDiff]; 
      } 
+};
+
+/*  Hashmap Solution
+    Time Complexity - O(n)
+    Space Complexity - O(n)
+*/
+var twoSum = function(nums, target) {
+    // edge case for empty array
+    if (!nums) return []
+
+    let map = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        if(map.has(target - nums[i])){
+            return [map.get(target - nums[i]), i ]
+        }else{
+            map.set(nums[i], i)
+        }   
+    }
+
+    return []
 };
